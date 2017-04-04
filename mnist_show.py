cdb39cde8f5c14648b3912d3147640ded2ade755
TatsukiSerizawa <iputatsuki@gmail.com>
2017-04-05 01:41:19 +0900

    9 MNIST画像の表示

--- mnist_show.py
+++ mnist_show.py
@@ -4,12 +4,14 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 from mnist import load_mnist
-from PIL import Image
+from PIL import Image #PIL(Python Image Library)モジュールを使用
 
+#訓練画像の1枚目を表示
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
+#flatten=Trueで読み込んだ画像はNumPy配列として1次元で格納
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten = True, normalize = False)
 
 img = x_train[0]
@@ -17,7 +19,7 @@ label = t_train[0]
 print(label)
 
 print(img.shape)
-img = img.reshape(28, 28)
+img = img.reshape(28, 28) #28x28のサイズに再変形
 print(img.shape)
 
 img_show(img)
