56a9c8d900d68b8dba9c5c1c63176ea1a1e238af
TatsukiSerizawa <iputatsuki@gmail.com>
2017-04-10 00:21:36 +0900

    12,14 損失関数（2乗和誤差，[バッチ対応版含]交差エントロピー誤差）

--- loss_function.py
+++ loss_function.py
@@ -6,10 +6,28 @@ import numpy as np
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
+'''
 #損失関数(交差エントロピー誤差)
 def cross_entropy_error(y, t):
     delta = 1e-7 #log0となってしまうことを防ぐ
     return -np.sum(t * np.log(y + delta))
+
+#損失関数（[バッチ対応版]交差エントロピー誤差 教師データがラベルの時）
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+'''
+#損失関数（[バッチ対応版]交差エントロピー誤差）
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    delta = 1e-7 #log0となってしまうことを防ぐ
+    return -np.sum(t * np.log(y + delta)) / batch_size
 
 
 #教師データ(2が正解とする)
