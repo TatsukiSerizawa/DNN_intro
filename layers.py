d6b45b5b1cabf771e7044a34c94594736cddd629
TatsukiSerizawa <iputatsuki@gmail.com>
2017-05-25 00:16:54 +0900

    25 ReLUレイヤ，Sigmoidレイヤ

--- layers.py
+++ layers.py
@@ -20,6 +20,21 @@ class Relu:
         return dx
 
 
+#Sigmoidレイヤ
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+    
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout): #Sigmoidレイヤの計算を入力と出力のみに省略し，計算を効率化したもの
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
 x = np.array([[1.0, -0.5], [-2.0, 3.0]])
 print(x)
 
